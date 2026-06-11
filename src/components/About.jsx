91d3e9b0c2ac5307469b3c3c9bd89f387f501acf
Axelle <axell.castandet@gmail.com>
2026-06-11 19:04:14 +0200

    edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,17 @@ function About() {
         <section className="about" id="about">
             <h2>Qui suis-je ?</h2>
             <p>
-                Je suis une développeuse passionnée par la création d'expériences numériques innovantes. Avec une solide formation en développement web et une expérience pratique dans divers projets, je m'efforce de transformer des idées en applications fonctionnelles et esthétiques. Mon objectif est de contribuer à des projets qui ont un impact positif sur les utilisateurs tout en continuant à apprendre et à évoluer dans le domaine du développement.
+              Je m'appelle Axelle Castandet. Après avoir évolué dans différents secteurs tels que la restauration, l'aide à la personne et la logistique, j'ai développé des qualités essentielles comme le sens du travail en équipe, la rigueur et la conscience professionnelle. 
+              Ces expériences m'ont également appris à m'adapter, à être à l'écoute et à toujours m'investir pleinement dans les missions qui me sont confiées. <br></br>
+
+Passionnée par le numérique et attirée par la dimension créative du développement, j'ai choisi d'entreprendre une reconversion professionnelle vers le développement web. J'ai commencé par me former en autodidacte grâce à différentes ressources en ligne avant d'intégrer la formation Développeur Web et Web Mobile de l'AFPA de Bègles, où j'ai obtenu un titre professionnel de niveau 5 en mars 2026. <br></br>
+
+Au cours de cette formation, j'ai acquis des compétences en conception d'interfaces avec Figma, en développement front-end avec HTML, CSS et JavaScript, ainsi qu'en développement back-end avec PHP et Symfony. J'ai également découvert Vue.js et travaillé en méthode Agile/SCRUM à travers des sprints hebdomadaires. Cette expérience a confirmé mon intérêt pour un métier qui demande d'apprendre en permanence, de résoudre des problématiques concrètes et de collaborer avec différents profils.
+
+Aujourd'hui, je m'oriente vers le développement web full stack avec une appétence particulière pour React et Node.js. Mon portfolio, développé avec React, reflète mon intérêt pour la création d'interfaces modernes et d'expériences utilisateur soignées. Curieuse, créative et à l'écoute, j'apprécie autant les aspects techniques que la dimension humaine du métier. <br></br>
+
+Afin de poursuivre ma montée en compétences, j'ai choisi d'intégrer la Wild Code School en formation Concepteur Développeur d'Applications. Je suis actuellement à la recherche d'une alternance qui me permettra de mettre un pied dans le monde de l'entreprise, d'apprendre au sein d'un environnement collaboratif et de contribuer à des projets concrets tout en continuant à progresser en tant que développeuse web full stack.
+
             </p>
         </section>
     )
