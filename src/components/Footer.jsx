949be7b2aedf03c5258b9af288221d8ba03b0e6e
Axelle <axell.castandet@gmail.com>
2026-06-12 15:18:06 +0200

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 function Footer() {
     return (
         <footer className="footer">
-            <p>&copy; 2026 - Alnil Portfolio. Tous droits réservés.</p>
+            <p>&copy; 2026 - Alnilam Portfolio. Tous droits réservés.</p>
         </footer>
     )
 }
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -60,9 +60,22 @@ function TechBanner() {
             </section>
 
             <div className="about" id="about">
+                <h3> → En formation</h3>
                 <p>
-                    Toujours prête à apprendre de nouvelles technologies et à relever de nouveaux défis dans le domaine du développement web.
-                </p>
+                    Au cours de ma formation Développeur Web et Web Mobile, j'ai acquis des compétences aussi bien en conception qu'en développement.
+<br></br><br></br>
+J'ai notamment travaillé sur le maquettage d'interfaces avec Figma, le développement front-end avec HTML, CSS et JavaScript, ainsi que le développement back-end avec PHP et Symfony. J'ai également eu l'occasion de découvrir Vue.js et de réaliser plusieurs projets mettant en pratique ces différentes technologies.
+<br></br><br></br>
+Au-delà des aspects techniques, cette formation m'a permis de comprendre les différentes étapes de réalisation d'un projet web, de la rédaction du cahier des charges jusqu'à sa mise en production. J'ai également été initiée aux méthodes Agiles et au framework SCRUM, en travaillant en équipe au travers de sprints hebdomadaires.
+</p>
+<h3> → En stage </h3>
+<p>
+  Lors de mon stage, j'ai participé au développement de deux projets réalisés pour des clients réels, en collaboration avec mon maître de stage et les autres stagiaires. Cette expérience m'a permis de mettre en pratique les compétences acquises durant ma formation et de découvrir le fonctionnement d'un projet en conditions professionnelles.
+<br></br><br></br>
+J'ai contribué à la conception des interfaces avec Penpot, puis au développement des applications avec React pour le front-end et Node.js avec Express.js pour le back-end. J'ai également utilisé Git et GitHub pour le versionnement et le travail collaboratif.
+<br></br><br></br>
+Au-delà des compétences techniques, cette expérience a renforcé mon autonomie, ma capacité d'adaptation et mon aptitude à travailler en équipe. Elle m'a également permis d'échanger directement avec les clients et de mieux comprendre les enjeux liés à la conception et au développement de solutions répondant à leurs besoins.
+</p>
             </div>
         </>
     );
